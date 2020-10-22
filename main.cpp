037777367723d7928cf2c5241bc68d5e36356b00
Elliott Hebert <elliott.hebert@heig-vd.ch>
2020-10-22 14:49:14 +0200

    add calendar format

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 */
 #include <iostream>
 #include <climits>
+#include <iomanip>
 
 using namespace std;
 
@@ -22,6 +23,8 @@ using namespace std;
 int main() {
 	 const int MIN_YEAR = 1900,
 				MAX_YEAR = 2100;
+
+	 enum class Months { JAN=1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
 
 	 cout << "Bonjour" << endl
 			<< "Ce programme vous demande une annee et vous affiche les calendriers de cette annee" << endl;
@@ -44,7 +47,112 @@ int main() {
 				}
 		  } while (isIncorrect);
 
-		  cout << "Votre numéro est : " << userYear << endl;
+		  // cout << "Votre numéro est : " << userYear << endl;
+
+		  // Display calendar
+		  bool isUserYearLeap = (userYear % 4 == 0 or userYear % 400 == 0) and userYear % 100 != 0;
+		  short feblastDay = isUserYearLeap ? 29 : 28 ;
+		  short lastDay, firstDayOfMonth = 1;
+
+        for (int month = 1; month <= 12 ; ++month) {
+           switch (month) {
+             case 1:  {
+                lastDay = 31;
+                cout << "JANVIER";
+                break;
+             }
+             case 2:  {
+                lastDay = feblastDay;
+                cout << "FEVRIER";
+                break;
+             }
+             case 3: {
+                lastDay = 31;
+                cout << "MARS";
+                break;
+             }
+             case 4: {
+                lastDay = 30;
+                cout << "AVRIL";
+                break;
+             }
+             case 5: {
+                lastDay = 31;
+                cout << "MAI";
+                break;
+             }
+             case 6: {
+                lastDay = 30;
+                cout << "JUIN"
+                ; break;
+             }
+             case 7: {
+                lastDay = 31;
+                cout << "JUILLET";
+                break;
+             }
+             case 8: {
+                lastDay = 31;
+                cout << "AOUT";
+                break;
+             }
+             case 9: {
+                lastDay = 30;
+                cout << "SEPTEMBRE";
+                break;
+             }
+             case 10: {
+                lastDay = 31;
+                cout << "OCTOBRE";
+                break;
+             }
+             case 11: {
+                lastDay = 30;
+                cout << "NOVEMBRE";
+                break;
+             }
+             case 12:{
+                lastDay = 31;
+                cout << "DECEMBRE";
+                break;
+             }
+          }
+
+           cout << " " << userYear << endl;
+
+           short day = 1;
+
+           cout << left << setw(3) << "L"
+                << left << setw(3) << "M"
+                << left << setw(3) << "M"
+                << left << setw(3) << "J"
+                << left << setw(3) << "V"
+                << left << setw(3) << "S"
+                << left << setw(3) << "D"
+                << endl;
+           for (int week = 1; week <= 5; ++week) {
+
+              for (int dayOfWeek = 1; dayOfWeek <= 7; ++dayOfWeek) {
+                 if (day * week < firstDayOfMonth) {
+                    cout << left << setw(3) << "";
+                 } else {
+                    cout << left << setw(3) << day;
+                 }
+
+                  if (day == lastDay){
+                     firstDayOfMonth = dayOfWeek + 1;
+                     break;
+                  } else {
+                     ++day;
+                  }
+
+              }
+
+              cout << endl;
+
+           }
+           cout << endl;
+        }
 
 		  cout << "Voulez-vous recommencer ? [O/N]";
 		  cin >> quit;
